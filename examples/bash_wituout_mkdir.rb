de8c78cd6a355d25673bcd92496379bc008b825d
Uchio KONDO <udzura@pepabo.com>
2016-11-17 18:50:59 +0900

    Fix example

--- examples/bash_wituout_mkdir.rb
+++ examples/bash_wituout_mkdir.rb
@@ -1,7 +1,15 @@
 context = Seccomp.new(default: :allow) do |rule|
   rule.kill(:mkdir, Seccomp::ARG(:>=, 0), Seccomp::ARG(:>=, 0))
 end
-context.load
 
-exec "/bin/bash"
-# And then try to mkdir
+pid = Process.fork do
+  context.load
+
+  puts "==== It will be jailed. Please try to mkdir"
+  exec "/bin/sh"
+end
+
+p(Process.waitpid2 pid)
+
+puts "==== It will not be jailed"
+exec "/bin/sh"
