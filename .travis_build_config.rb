c8f87d4d5ef5a419d0666388ce5c0ae9f8bbc027
Uchio KONDO <udzura@pepabo.com>
2016-11-17 18:04:10 +0900

    Add gems useful for test

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -1,6 +1,10 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
+
+  conf.gem mgem: 'mruby-process'
+  conf.gem github: 'haconiwa/mruby-exec'
+
   conf.gem '../mruby-seccomp'
   conf.enable_test
 end
